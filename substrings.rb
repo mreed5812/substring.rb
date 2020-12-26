cb87bca349f036200982765dcd1274103764ffea
Michael Reed <mreed5812@gmail.com>
2020-12-26 15:22:07 -0600

    first commit

---
+++ substrings.rb
@@ -0,0 +1,8 @@
+#Recall how to turn strings into arrays and arrays into strings
+#method substrings should take a word as first argument and then array of valid substrings as the second argument
+#Should be case INSENSITIVE
+
+
+def substrings(string, dictionary)
+    #do some stuff
+end
